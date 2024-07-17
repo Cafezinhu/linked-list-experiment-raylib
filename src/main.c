ce865bbe2439d4cc4c077af16a5c4aed478456d5
Cafezinhu <aertoncastro@gmail.com>
2024-07-17 20:54:04 -0300

    change direction

--- src/main.c
+++ src/main.c
@@ -26,6 +26,8 @@ struct Entity {
   struct Entity *next_entity;
   float x;
   float y;
+  float scaleX;
+  float scaleY;
   Texture texture;
   bool dead;
 
@@ -36,19 +38,30 @@ struct Entity *first_entity;
 
 void UpdateBullet(struct Entity *bullet);
 void UpdateBullet(struct Entity *bullet) {
-  (*bullet).x += BULLET_SPEED * GetFrameTime();
-  DrawTexture(bullet->texture, bullet->x, bullet->y, WHITE);
-  if (bullet->x > screenWidth) {
+  (*bullet).x += BULLET_SPEED * GetFrameTime() * bullet->scaleX;
+  Rectangle source_rec = {bullet->texture.width, 0,
+                          bullet->texture.width * bullet->scaleX,
+                          bullet->texture.height};
+
+  Rectangle dest_rec = {bullet->x, bullet->y, bullet->texture.width,
+                        bullet->texture.height};
+
+  DrawTexturePro(bullet->texture, source_rec, dest_rec, (Vector2){0, 0}, 0,
+                 WHITE);
+
+  if (bullet->x > screenWidth || bullet->x < 0) {
     (*bullet).dead = true;
   }
 }
 
-struct Entity CreateBullet(int x, int y, struct Entity *next_entity);
-struct Entity CreateBullet(int x, int y, struct Entity *next_entity) {
+struct Entity CreateBullet(int x, int y, int dir, struct Entity *next_entity);
+struct Entity CreateBullet(int x, int y, int dir, struct Entity *next_entity) {
   struct Entity bullet = {};
 
   bullet.x = x;
   bullet.y = y;
+  bullet.scaleX = (float)dir;
+  bullet.scaleY = 1.0;
   bullet.texture = bullet_texture;
   bullet.next_entity = next_entity;
   bullet.Update = &UpdateBullet;
@@ -62,8 +75,10 @@ void UpdatePlayer(struct Entity *player) {
 
   if (IsKeyDown(KEY_D)) {
     vec.x = 1;
+    (*player).scaleX = 1;
   } else if (IsKeyDown(KEY_A)) {
     vec.x = -1;
+    (*player).scaleX = -1;
   }
 
   if (IsKeyDown(KEY_W)) {
@@ -79,12 +94,22 @@ void UpdatePlayer(struct Entity *player) {
   (*player).x += vec.x * PLAYER_SPEED * GetFrameTime();
   (*player).y += vec.y * PLAYER_SPEED * GetFrameTime();
 
-  DrawTexture(player->texture, player->x, player->y, WHITE);
+  Rectangle source_rec = {player->texture.width, 0,
+                          player->texture.width * player->scaleX,
+                          player->texture.height};
+
+  Rectangle dest_rec = {player->x, player->y, player->texture.width,
+                        player->texture.height};
+
+  DrawTexturePro(player->texture, source_rec, dest_rec, (Vector2){0, 0}, 0,
+                 WHITE);
+  // DrawTexture(player->texture, player->x, player->y, WHITE);
 
   if (IsKeyPressed(KEY_SPACE)) {
     struct Entity *old_next = player->next_entity;
     (*player).next_entity = malloc(sizeof(struct Entity));
-    *(*player).next_entity = CreateBullet(player->x, player->y, old_next);
+    *(*player).next_entity =
+        CreateBullet(player->x, player->y, player->scaleX, old_next);
   }
 }
 
@@ -94,6 +119,8 @@ struct Entity CreatePlayer() {
 
   player.texture = LoadTexture("boneco.png");
   player.Update = &UpdatePlayer;
+  player.scaleX = 1.0;
+  player.scaleY = 1.0;
 
   return player;
 }
