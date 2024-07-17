184d2d07949502f0abab4c5798c68894072f82fd
Cafezinhu <aertoncastro@gmail.com>
2024-07-16 23:48:14 -0300

    linked list

--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 #if defined(PLATFORM_DESKTOP)
 #define GLSL_VERSION 330
@@ -17,22 +18,46 @@
 const int screenWidth = 800;
 const int screenHeight = 600;
 const int PLAYER_SPEED = 300;
+const int BULLET_SPEED = 300;
+
+Texture bullet_texture;
 
 struct Entity {
+  struct Entity *next_entity;
   float x;
   float y;
   Texture texture;
+  bool dead;
 
-  void (*Update)(struct Entity *entity);
+  void (*Update)(struct Entity *player);
 };
 
-struct Item {
-  struct Item *next;
-  struct Entity entity;
-};
+struct Entity *first_entity;
 
-void UpdatePlayer(struct Entity *entity);
-void UpdatePlayer(struct Entity *entity) {
+void UpdateBullet(struct Entity *bullet);
+void UpdateBullet(struct Entity *bullet) {
+  (*bullet).x += BULLET_SPEED * GetFrameTime();
+  DrawTexture(bullet->texture, bullet->x, bullet->y, WHITE);
+  if (bullet->x > screenWidth) {
+    (*bullet).dead = true;
+  }
+}
+
+struct Entity CreateBullet(int x, int y, struct Entity *next_entity);
+struct Entity CreateBullet(int x, int y, struct Entity *next_entity) {
+  struct Entity bullet = {};
+
+  bullet.x = x;
+  bullet.y = y;
+  bullet.texture = bullet_texture;
+  bullet.next_entity = next_entity;
+  bullet.Update = &UpdateBullet;
+
+  return bullet;
+}
+
+void UpdatePlayer(struct Entity *player);
+void UpdatePlayer(struct Entity *player) {
   Vector2 vec = {};
 
   if (IsKeyDown(KEY_D)) {
@@ -51,28 +76,31 @@ void UpdatePlayer(struct Entity *entity) {
     vec = Vector2Normalize(vec);
   }
 
-  (*entity).x += vec.x * PLAYER_SPEED * GetFrameTime();
-  (*entity).y += vec.y * PLAYER_SPEED * GetFrameTime();
+  (*player).x += vec.x * PLAYER_SPEED * GetFrameTime();
+  (*player).y += vec.y * PLAYER_SPEED * GetFrameTime();
 
-  DrawTexture(entity->texture, entity->x, entity->y, WHITE);
+  DrawTexture(player->texture, player->x, player->y, WHITE);
+
+  if (IsKeyPressed(KEY_SPACE)) {
+    struct Entity *old_next = player->next_entity;
+    (*player).next_entity = malloc(sizeof(struct Entity));
+    *(*player).next_entity = CreateBullet(player->x, player->y, old_next);
+  }
 }
 
 struct Entity CreatePlayer();
 struct Entity CreatePlayer() {
   struct Entity player = {};
 
-  player.x = 0;
-  player.y = 0;
   player.texture = LoadTexture("boneco.png");
   player.Update = &UpdatePlayer;
 
   return player;
 }
 
-void UpdateDrawFrame(struct Item *first_item);
+void UpdateDrawFrame(struct Entity *first_entity);
 
 //------------------ASSETS--------------
-Texture tex;
 
 int main() {
 
@@ -91,16 +119,18 @@ int main() {
   emscripten_set_main_loop(UpdateDrawFrame, 0, 1);
 #else
   // SetTargetFPS(60);
-  tex = LoadTexture("boneco.png");
-  struct Item *first_item = (struct Item *)malloc(sizeof(struct Item));
+  // struct Entity *first_entity = (struct Entity *)malloc(sizeof(struct
+  // player));
+  bullet_texture = LoadTexture("bala.png");
+  first_entity = malloc(sizeof(struct Entity));
 
-  *first_item = (struct Item){};
-  (*first_item).entity = CreatePlayer();
+  *first_entity = (struct Entity){};
+  *first_entity = CreatePlayer();
   // #ifndef PLATFORM_ANDROID
   // ChangeDirectory("..");
   // #endif
   while (!WindowShouldClose()) {
-    UpdateDrawFrame(first_item);
+    UpdateDrawFrame(first_entity);
   }
 #endif
 
@@ -108,13 +138,31 @@ int main() {
   return 0;
 }
 
-void UpdateDrawFrame(struct Item *first_item) {
+void UpdateDrawFrame(struct Entity *first_entity) {
   // Draw
   //----------------------------------------------------------------------------------
   BeginDrawing();
   ClearBackground(RAYWHITE);
   DrawFPS(10, 10);
-  first_item->entity.Update(&(first_item->entity));
+  struct Entity *entity = first_entity;
+  struct Entity *previous_entity = NULL;
+  while (entity != NULL) {
+    if (entity->dead) {
+      printf("Killing entity\n");
+      struct Entity *next_entity = entity->next_entity;
+      if (first_entity != NULL && previous_entity != NULL) {
+        (*previous_entity).next_entity = entity->next_entity;
+        entity = entity->next_entity;
+      } else {
+        first_entity = next_entity;
+      }
+      free(entity);
+    } else {
+      entity->Update(entity);
+      previous_entity = entity;
+      entity = entity->next_entity;
+    }
+  }
 
   EndDrawing();
 }
